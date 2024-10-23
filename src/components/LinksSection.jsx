60866b3bd6ea3181f62c0fc7ac893b4e9618f080
Tamara Yustian <tamarayustian@gmail.com>
2024-10-23 14:33:26 +0800

    refactor: change width and gap

--- src/components/LinksSection.jsx
+++ src/components/LinksSection.jsx
@@ -4,10 +4,10 @@ const LinksSection = ({ className, linksList }) => {
   return (
     <div className={className}>
       <h2 className="pb-10 uppercase text-h3">Links</h2>
-      <div className="flex flex-col gap-y-5 gap-x-10 justify-center items-center w-full lg:flex-row lg:gap-10">
+      <div className="flex flex-col gap-y-5 gap-x-10 w-full lg:flex-row lg:gap-10">
         {linksList.map(({ title, target }) => (
           <a
-            className="flex items-center justify-center py-2 px-5 rounded-lg font-heading font-normal text-center border-4 border-orange-600 shadow-md hover:bg-orange-600 hover:border-orange-600 w-full text-nowrap"
+            className="flex items-center justify-center py-2 px-5 rounded-lg font-heading text-center border-4 border-orange-500 shadow-md hover:bg-orange-500 hover:border-orange-500 min-w-fit text-nowrap"
             href={target}
             key={title}
             rel="noreferrer noopener"
