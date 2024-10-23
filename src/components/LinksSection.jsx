496ab7eff78d531349ef1568dd7c1fd2f516eff3
Tamara Yustian <tamarayustian@gmail.com>
2024-10-23 14:26:08 +0800

    refactor: use text-h3 class

--- src/components/LinksSection.jsx
+++ src/components/LinksSection.jsx
@@ -3,13 +3,13 @@ import PropTypes from "prop-types";
 const LinksSection = ({ className, linksList }) => {
   return (
     <div className={className}>
-      <h3 className="pb-10 text-2xl font-bold uppercase font-heading">Links</h3>
-      <div className="flex flex-col gap-5 justify-center items-center w-full lg:flex-row lg:gap-10">
+      <h2 className="pb-10 uppercase text-h3">Links</h2>
+      <div className="flex flex-col gap-y-5 gap-x-10 justify-center items-center w-full lg:flex-row lg:gap-10">
         {linksList.map(({ title, target }) => (
           <a
             className="flex items-center justify-center py-2 px-5 rounded-lg font-heading font-normal text-center border-4 border-orange-600 shadow-md hover:bg-orange-600 hover:border-orange-600 w-full text-nowrap"
-            href={title}
-            key={target}
+            href={target}
+            key={title}
             rel="noreferrer noopener"
             target="_blank"
           >
