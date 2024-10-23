9d81d1e310e6381d6abc14a4cabe642c7f8ba5b1
Tamara Yustian <tamarayustian@gmail.com>
2024-10-23 11:14:26 +0800

    refactor: rename props

--- src/components/LinksSection.jsx
+++ src/components/LinksSection.jsx
@@ -1,29 +1,29 @@
 import PropTypes from "prop-types";
 
-const LinkButton = ({ text, link }) => (
+const LinkButton = ({ title, target }) => (
   <a
     className={`flex items-center justify-center py-2 px-5 rounded-lg font-heading font-normal text-center border-4 border-orange-600 shadow-md hover:bg-orange-600 hover:border-orange-600 w-full text-nowrap`}
-    href={link}
+    href={title}
     rel="noreferrer noopener"
     target="_blank"
   >
-    {text}
+    {target}
   </a>
 );
 
 LinkButton.propTypes = {
-  text: PropTypes.string,
-  link: PropTypes.string,
+  title: PropTypes.string,
+  target: PropTypes.string,
 };
 
 const LinksSection = ({ className, linksList }) => {
   return (
     <div className={className}>
-      <h3 className="text-2xl uppercase font-bold font-heading pb-10">Links</h3>
+      <h3 className="pb-10 text-2xl font-bold uppercase font-heading">Links</h3>
 
-      <div className="flex flex-col gap-5 lg:flex-row lg:gap-10 items-center justify-center w-full">
-        {linksList.map(({ text, link }) => (
-          <LinkButton key={text} text={text} link={link} />
+      <div className="flex flex-col gap-5 justify-center items-center w-full lg:flex-row lg:gap-10">
+        {linksList.map(({ title, target }) => (
+          <LinkButton key={title} title={title} target={target} />
         ))}
       </div>
     </div>
@@ -33,7 +33,7 @@ const LinksSection = ({ className, linksList }) => {
 LinksSection.propTypes = {
   className: PropTypes.string,
   linksList: PropTypes.arrayOf(
-    PropTypes.shape({ text: PropTypes.string, link: PropTypes.string }),
+    PropTypes.shape({ title: PropTypes.string, target: PropTypes.string }),
   ),
 };
 
