bc67331feb89256a6ba70caa80d9b67fe2f9e283
Tamara Yustian <tamarayustian@gmail.com>
2024-10-28 23:52:25 +0800

    feat: add Respond 123Go box

---
+++ src/components/Go123Box.jsx
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types";
+import logoPath from "../assets/respond-aa2023.svg";
+
+const Go123Box = ({ className, description, bgColor, textColor }) => {
+  return (
+    <div className={className}>
+      <div
+        className={`${bgColor} flex flex-col justify-center items-center py-5 px-2 rounded-lg text-center`}
+      >
+        <p className={`text-center text-p ${textColor}`}>{description}</p>
+        <img src={logoPath} alt="Arise Asia response logo" className="w-36" />
+      </div>
+    </div>
+  );
+};
+
+Go123Box.propTypes = {
+  className: PropTypes.string,
+  description: PropTypes.string,
+  bgColor: PropTypes.string.isRequired,
+  textColor: PropTypes.string.isRequired,
+};
+
+export default Go123Box;
---
+++ src/stories/Go123Box.stories.jsx
@@ -0,0 +1,14 @@
+import { Go123Box } from "../components";
+
+export default {
+  title: "Go123 Box",
+  component: Go123Box,
+};
+
+export const Default = {
+  args: {
+    description: "Responding to",
+    bgColor: "bg-purple-200",
+    textColor: "text-black",
+  },
+};
