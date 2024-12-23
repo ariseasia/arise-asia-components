b811f4edc038db39b7fb16926b66b5807e250c22
Tamara Yustian <tamarayustian@gmail.com>
2024-12-23 15:00:55 +0800

    feat: add moments carousel

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 export { default as Footer } from "./Footer";
 export { default as HeroSection } from "./HeroSection";
 export { default as LinksSection } from "./LinksSection";
+export { default as MomentsCarousel } from "./MomentsCarousel";
 export { default as MomentsSection } from "./MomentsSection";
 export { default as Navbar } from "./Navbar";
 export { default as ProgramCarousel } from "./ProgramCarousel";
