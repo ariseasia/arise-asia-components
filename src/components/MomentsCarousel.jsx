f552c3ed13a0753b51338674b392ea47b3868c74
Tamara Yustian <tamarayustian@gmail.com>
2024-12-24 10:50:12 +0800

    feat: centered arrow buttons

--- src/components/MomentsCarousel.jsx
+++ src/components/MomentsCarousel.jsx
@@ -20,7 +20,7 @@ const MomentsCarousel = ({
     <div className={className}>
       <div className="relative">
         <IoIosArrowDropleft
-          className={`absolute left-0 top-1/3 ${textColor} ${bgColor} rounded-full -translate-x-1/2 ${hoverBgColor} drop-shadow-2xl`}
+          className={`absolute left-0 top-2/5 md:top-1/2 ${textColor} ${bgColor} rounded-full -translate-x-1/2 ${hoverBgColor} drop-shadow-2xl`}
           onClick={handleLeft}
           size={36}
         />
@@ -30,23 +30,23 @@ const MomentsCarousel = ({
           src={moments[activeIdx].image}
         />
         <IoIosArrowDropright
-          className={`absolute right-0 top-1/3 ${textColor} ${bgColor} rounded-full translate-x-1/2 ${hoverBgColor} drop-shadow-2xl`}
+          className={`absolute right-0 top-2/5 md:top-1/2 ${textColor} ${bgColor} rounded-full translate-x-1/2 ${hoverBgColor} drop-shadow-2xl`}
           onClick={handleRight}
           size={36}
         />
-        <div className="w-full h-40">
-          <article
-            className={`py-4 pr-16 pl-5 ${textColor} ${bgColor} rounded-xl sm:py-6 sm:pl-8 [clip-path:polygon(0_0,_0_100%,_80%_100%,_100%_75%,_100%_0)]`}
-          >
-            <p className="italic text-p">{moments[activeIdx].day}</p>
-            <h4 className="mb-1 font-bold leading-tight sm:mb-2 sm:text-2xl">
-              {moments[activeIdx].title}
-            </h4>
-            <p className={`${textColor} text-p`}>
-              {moments[activeIdx].description}
-            </p>
-          </article>
-        </div>
+      </div>
+      <div className="w-full h-40">
+        <article
+          className={`py-4 pr-16 pl-5 ${textColor} ${bgColor} rounded-xl sm:py-6 sm:pl-8 [clip-path:polygon(0_0,_0_100%,_80%_100%,_100%_75%,_100%_0)]`}
+        >
+          <p className="italic text-p">{moments[activeIdx].day}</p>
+          <h4 className="mb-1 font-bold leading-tight sm:mb-2 sm:text-2xl">
+            {moments[activeIdx].title}
+          </h4>
+          <p className={`${textColor} text-p`}>
+            {moments[activeIdx].description}
+          </p>
+        </article>
       </div>
     </div>
   );
