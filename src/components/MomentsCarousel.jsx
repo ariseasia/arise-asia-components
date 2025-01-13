d1fcea4e7fcbd99f57c99201b3394febbfaf3f81
tamarayustian <tamarayustian@gmail.com>
2025-01-14 00:18:27 +0800

    feat: add moments carousel (#9)
    * feat: add moments carousel
    
    * feat: add samples
    
    * feat: centered arrow buttons
    
    * fix: rounded corners not cut-off
    
    * feat: change to momentsData from moments
    
    * feat: add calculation for max height in description
    
    * fix: adjust description to be centered

---
+++ src/components/MomentsCarousel.jsx
@@ -0,0 +1,104 @@
+import PropTypes from "prop-types";
+import { useEffect, useRef, useState } from "react";
+import { IoIosArrowDropleft, IoIosArrowDropright } from "react-icons/io";
+
+const MomentsCarousel = ({
+  className,
+  bgColor,
+  hoverBgColor,
+  textColor = "text-white",
+  momentsData,
+}) => {
+  const [activeIdx, setActiveIdx] = useState(0);
+  const len = momentsData.length;
+
+  const handleLeft = () => setActiveIdx((activeIdx + 1) % len);
+
+  const handleRight = () => setActiveIdx((activeIdx - 1 + len) % len);
+
+  const descriptionRef = useRef(null);
+  const [maxHeight, setMaxHeight] = useState(0);
+
+  const calculateMaxHeight = () => {
+    const heights = momentsData.map((data) => {
+      if (!data.description) return 0;
+      const div = document.createElement("div");
+      div.style.position = "absolute";
+      div.style.visibility = "hidden";
+      div.style.height = "auto";
+      div.style.width = descriptionRef.current.offsetWidth + "px";
+      div.innerHTML = data.description;
+      document.body.appendChild(div);
+      const height = div.offsetHeight;
+      document.body.removeChild(div);
+      return height;
+    });
+    setMaxHeight(Math.max(...heights));
+  };
+
+  useEffect(() => {
+    calculateMaxHeight();
+    window.addEventListener("resize", calculateMaxHeight);
+    return () => window.removeEventListener("resize", calculateMaxHeight);
+  }, [momentsData]);
+
+  return (
+    <div className={className}>
+      <div className="relative">
+        <IoIosArrowDropleft
+          className={`absolute left-0 top-2/5 md:top-1/2 ${textColor} ${bgColor} rounded-full -translate-x-1/2 ${hoverBgColor} drop-shadow-2xl`}
+          onClick={handleLeft}
+          size={36}
+        />
+        <div className="py-5">
+          <img
+            alt={momentsData[activeIdx].title}
+            className="object-cover m-auto w-full h-full rounded-xl aspect-video"
+            src={momentsData[activeIdx].image}
+          />
+        </div>
+        <IoIosArrowDropright
+          className={`absolute right-0 top-2/5 md:top-1/2 ${textColor} ${bgColor} rounded-full translate-x-1/2 ${hoverBgColor} drop-shadow-2xl`}
+          onClick={handleRight}
+          size={36}
+        />
+      </div>
+      <div className="w-full h-40">
+        <article
+          className={`py-4 pr-16 pl-5 ${textColor} ${bgColor} rounded-xl sm:py-6 sm:pl-8 [clip-path:polygon(0_0,_0_100%,_80%_100%,_100%_75%,_100%_0)]`}
+        >
+          <div className="flex flex-col justify-between">
+            <p className="italic text-p">{momentsData[activeIdx].day}</p>
+            <h4 className="mb-1 font-bold leading-tight sm:mb-2 sm:text-2xl">
+              {momentsData[activeIdx].title}
+            </h4>
+            <p
+              ref={descriptionRef}
+              className={`${textColor} text-p flex items-center`}
+              style={{ height: `${maxHeight}px` }}
+            >
+              {momentsData[activeIdx].description}
+            </p>
+          </div>
+        </article>
+      </div>
+    </div>
+  );
+};
+
+MomentsCarousel.propTypes = {
+  className: PropTypes.string,
+  bgColor: PropTypes.string.isRequired,
+  hoverBgColor: PropTypes.string.isRequired,
+  textColor: PropTypes.string,
+  momentsData: PropTypes.arrayOf(
+    PropTypes.shape({
+      day: PropTypes.string,
+      title: PropTypes.string.isRequired,
+      image: PropTypes.string.isRequired,
+      description: PropTypes.string,
+    }),
+  ).isRequired,
+};
+
+export default MomentsCarousel;
---
+++ src/stories/MomentsCarousel.stories.jsx
@@ -0,0 +1,146 @@
+import MomentsCarousel from "../components/MomentsCarousel";
+
+export default {
+  title: "Moments Carousel",
+  component: MomentsCarousel,
+};
+
+export const Default = {
+  args: {
+    bgColor: "bg-purple-500",
+    hoverBgColor: "hover:bg-purple-700",
+    momentsData: [
+      {
+        day: "Day 1",
+        title: "Fellowship Night",
+        description:
+          "A lively evening of fellowship, performances, and snacks, bringing people together in the hotel lounge",
+        image:
+          "https://images.unsplash.com/photo-1659301254614-8d6a9d46f26a?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
+      {
+        day: "Day 1",
+        title: "Afternoon Panel",
+        description:
+          "Panelists shared their perspectives on key justice challenges in the world today, focusing on their impact in global missions",
+        image:
+          "https://images.unsplash.com/photo-1607969892192-8aa9fe65ee26?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8d29ya3Nob3B8ZW58MHx8MHx8fDA%3D",
+      },
+      {
+        day: "Day 2",
+        title: "Missions Booths",
+        description:
+          "Missions partners and organizations showcased their ministries in booths surrounding the main hall",
+        image:
+          "https://images.unsplash.com/photo-1477281765962-ef34e8bb0967?q=80&w=2833&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
+      {
+        day: "Day 2",
+        title: "Innovation Lab",
+        description:
+          "Participants collaborated with their teams during the Innovation Lab, a hackathon for pitching innovative ideas for global missions",
+        image:
+          "https://images.unsplash.com/photo-1508829040592-72f179f8a73f?q=80&w=2952&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
+    ],
+  },
+};
+
+export const NoDay = {
+  args: {
+    bgColor: "bg-purple-500",
+    hoverBgColor: "hover:bg-purple-700",
+    momentsData: [
+      {
+        title: "Fellowship Night",
+        description:
+          "A lively evening of fellowship, performances, and snacks, bringing people together in the hotel lounge",
+        image:
+          "https://images.unsplash.com/photo-1659301254614-8d6a9d46f26a?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
+      {
+        title: "Afternoon Panel",
+        description:
+          "Panelists shared their perspectives on key justice challenges in the world today, focusing on their impact in global missions",
+        image:
+          "https://images.unsplash.com/photo-1607969892192-8aa9fe65ee26?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8d29ya3Nob3B8ZW58MHx8MHx8fDA%3D",
+      },
+      {
+        title: "Missions Booths",
+        description:
+          "Missions partners and organizations showcased their ministries in booths surrounding the main hall",
+        image:
+          "https://images.unsplash.com/photo-1477281765962-ef34e8bb0967?q=80&w=2833&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
+      {
+        title: "Innovation Lab",
+        description:
+          "Participants collaborated with their teams during the Innovation Lab, a hackathon for pitching innovative ideas for global missions",
+        image:
+          "https://images.unsplash.com/photo-1508829040592-72f179f8a73f?q=80&w=2952&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
+    ],
+  },
+};
+
+export const NoDescription = {
+  args: {
+    bgColor: "bg-purple-500",
+    hoverBgColor: "hover:bg-purple-700",
+    momentsData: [
+      {
+        day: "Day 1",
+        title: "Fellowship Night",
+        image:
+          "https://images.unsplash.com/photo-1659301254614-8d6a9d46f26a?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
+      {
+        day: "Day 1",
+        title: "Afternoon Panel",
+        image:
+          "https://images.unsplash.com/photo-1607969892192-8aa9fe65ee26?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8d29ya3Nob3B8ZW58MHx8MHx8fDA%3D",
+      },
+      {
+        day: "Day 2",
+        title: "Missions Booths",
+        image:
+          "https://images.unsplash.com/photo-1477281765962-ef34e8bb0967?q=80&w=2833&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
+      {
+        day: "Day 2",
+        title: "Innovation Lab",
+        image:
+          "https://images.unsplash.com/photo-1508829040592-72f179f8a73f?q=80&w=2952&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
+    ],
+  },
+};
+
+export const NoDayAndDescription = {
+  args: {
+    bgColor: "bg-purple-500",
+    hoverBgColor: "hover:bg-purple-700",
+    momentsData: [
+      {
+        title: "Fellowship Night",
+        image:
+          "https://images.unsplash.com/photo-1659301254614-8d6a9d46f26a?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
+      {
+        title: "Afternoon Panel",
+        image:
+          "https://images.unsplash.com/photo-1607969892192-8aa9fe65ee26?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8d29ya3Nob3B8ZW58MHx8MHx8fDA%3D",
+      },
+      {
+        title: "Missions Booths",
+        image:
+          "https://images.unsplash.com/photo-1477281765962-ef34e8bb0967?q=80&w=2833&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
+      {
+        title: "Innovation Lab",
+        image:
+          "https://images.unsplash.com/photo-1508829040592-72f179f8a73f?q=80&w=2952&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      },
+    ],
+  },
+};
