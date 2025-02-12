fd943a1e6e8a6cbddfc4e344dff5b01e4c9eafb9
Tamara Yustian <tamarayustian@gmail.com>
2025-02-12 19:02:44 +0800

    feat: create Values storybook component

---
+++ src/stories/ValuesSection.stories.jsx
@@ -0,0 +1,10 @@
+import ValuesSection from "../components/ValuesSection";
+
+export default {
+  title: "Values Section",
+  component: ValuesSection,
+};
+
+export const Default = {
+  args: {},
+};
